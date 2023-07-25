461828de69e418be954b5cefac1aafe206c5d206
Prasoon Shukla <prasoon.shukla@squareboat.com>
2023-07-25 14:08:08 +0530

    fix: add auth endpoint and api call

--- samples/sample-app/server/server.js
+++ samples/sample-app/server/server.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const cors = require('cors');
 const ImageKit = require('imagekit');
 const uuid = require('uuid');
-const fs = require('fs');
 const path = require('path');
 
 const pugTemplatePath = path.join(__dirname, "../views/index.pug");
@@ -21,15 +20,24 @@ const startServer = (port = 3000, PUBLIC_KEY, PRIVATE_KEY, URL_ENDPOINT) => {
                 urlEndpoint: URL_ENDPOINT
             });
 
-            router.get("/", (req, res) => {
+            router.get("/auth", (req, res) => {
                 try {
-                    // Generating security parameters.
-                    // For generating token, signature and expire again just refresh the page.
                     const token = req.query.token || uuid.v4();
                     const expiration = req.query.expire || parseInt(Date.now()/1000)+ (60 * 10); // Default expiration in 10 mins
+    
                     const signatureObj = imagekit.getAuthenticationParameters(token, expiration);
+    
+                    res.status(200).send(signatureObj);
+    
+                } catch (err) {
+                    console.error("Error while responding to auth request:", JSON.stringify(err, undefined, 2));
+                    res.status(500).send("Internal Server Error");
+                }
+            });
 
-                    res.render(pugTemplatePath, {publicKey: PUBLIC_KEY, urlEndpoint: URL_ENDPOINT, signatureObj});
+            router.get("/", (req, res) => {
+                try {
+                    res.render(pugTemplatePath, {publicKey: PUBLIC_KEY, urlEndpoint: URL_ENDPOINT, authenticationEndpoint: `http://localhost:3000/auth`});
                 } catch (err) {
                     console.error("Error while responding to static page request:", JSON.stringify(err, undefined, 2));
                     res.status(500).send("Internal Server Error");
